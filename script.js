cbed58ad440e37a31c5a60a80ad1c7bae24ed261
CCMPacheco <pachecoccmp@gmail.com>
2022-04-10 11:37:52 -0300

    Add change color on mouseover

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function createGrid(squares) {
-  clearGrid();
   if (squares > 0 && squares <= 64) {
+    clearGrid();
     container.style.cssText = `grid-template-columns: repeat(${squares}, 1fr); grid-template-rows: repeat(${squares}, 1fr)`;
     for (let i = 0; i < squares * squares; i++) {
       const divs = document.createElement("div");
+      divs.classList.add("grid-div");
       container.appendChild(divs);
     }
+    const gridDiv = document.querySelectorAll(".grid-div");
+    gridDiv.forEach((div) => {
+      div.addEventListener(
+        "mouseover",
+        () => (div.style.cssText = `background-color: black`)
+      );
+    });
   }
 }
 
@@ -16,3 +24,5 @@ function clearGrid() {
 }
 
 const container = document.querySelector(".container");
+
+createGrid(16);
